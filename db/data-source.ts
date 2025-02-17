60fc2d66a6a942169eb5a2c251c614e2d5f3ae96
Fernando-Leon <leon.monroy.fernando@gmail.com>
2025-02-17 10:47:28 -0600

    deploy in railway

--- db/data-source.ts
+++ db/data-source.ts
@@ -2,15 +2,23 @@ import { DataSource, DataSourceOptions } from "typeorm";
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'mysql',
-  host: 'localhost',
+  host: 'mysqlprod.mysql.database.azure.com',
   port: 3306,
-  username: 'root',
-  password: 'root',
+  username: 'ferleon',
+  password: 'rootP@12',
   database: 'database_tortilleria',
   entities: ['dist/**/*.entity{.ts,.js}'],
   migrations: ['dist/db/migrations/*{.ts,.js}'],
   //synchronize: true,
 }
+
+/**
+ * 
+ * hostname=mysqlprod.mysql.database.azure.com
+port=3306
+username=ferleon
+password={your-password}
+ */
 
 const dataSource = new DataSource(dataSourceOptions);
 export default dataSource;
